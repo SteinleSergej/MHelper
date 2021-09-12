500a0669af5605bf1544f221666653ac51018746
Sergej Steinle <steinle.sergej.prog@gmail.com>
2021-09-12 15:29:40 +0200

    add button logic

--- metric.py
+++ metric.py
@@ -1,6 +1,7 @@
 
 import tkinter
 from tkinter import StringVar, font
+from typing import Text
 
 #define window
 root= tkinter.Tk()
@@ -15,6 +16,26 @@ button_color = "#f5cf87"
 root.config(bg=bg_color)
 
 #Define functions
+def convert():
+    output_field.delete(0,tkinter.END)
+    convertion_values = {
+        "Byte": 10**0,
+        "kByte": 10**3,
+        "MByte": 10**6,
+        "GByte": 10**9
+    }
+
+    #Get user informations
+    start_value = float(input_field.get())
+    start_prefix = input_choice.get()
+    end_prefix = output_choice.get()
+
+    if start_value < 0 :
+        print("Error")
+    else:
+        value = start_value * convertion_values[start_prefix] / convertion_values[end_prefix]
+        print(value)
+        output_field.insert(0,value)
 
 
 #Define layout
@@ -26,11 +47,11 @@ input_field.grid(row=0,column=0,padx=10,pady=10)
 equal_label.grid(row=0,column=1)
 output_field.grid(row=0,column=2,padx=10,pady=10)
 
-input_field.insert(0,"The input comes here")
-
+input_field.insert(0,"1")
+input_field.focus()
 
 #Create dropdowns for values
-bin_list = ["Bit","Byte","KByte","MByte","GByte"]
+bin_list = ["Byte","kByte","MByte","GByte"]
 input_choice = StringVar()
 output_choice = StringVar()
 
@@ -41,11 +62,11 @@ input_dropdown.grid(row=1,column=0,sticky="EW",pady=(10,4),padx=10)
 output_dropdown.grid(row=1,column=2,sticky="EW",pady=(10,4),padx=10)
 
 #set standard i/0-choices
-input_choice.set("Bit")
-output_choice.set("Byte")
+input_choice.set("Byte")
+output_choice.set("kByte")
 
 #create convert button
-convert_button = tkinter.Button(root,text="Convert",font=field_font,bg=button_color)
+convert_button = tkinter.Button(root,text="Convert",font=field_font,bg=button_color,command=convert)
 
 convert_button.grid(row=2,column=0,columnspan=3,sticky="NESW")
 #Run the root window
